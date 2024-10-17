975846be7e4e0f8fbe6a16a3fa85f5a713c3115f
Suyog3640 <119496630+Suyog3640@users.noreply.github.com>
2024-10-17 12:52:17 +0530

    Update index.js

--- chatbot-be/index.js
+++ chatbot-be/index.js
@@ -9,7 +9,13 @@ dotenv.config();
 connectDB();
 const app = express();
 
-app.use(cors());
+app.use(cors(
+    {
+        origin: ["https://chatbot-frontend-smoky.vercel.app/"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
 app.use(express.json());
 
 app.use('/', router);
